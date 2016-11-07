4c6cae9061a4b8956d887ffc92cdacffb5f38084
kera4214 <at.kylee-xohaa18@hotmail.co.jp>
2016-11-07 19:38:48 +0900

    Update FizzBuzz.java
    add comments

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,15 +1,14 @@
 class FizzBuzz{
   public static void main(String[] args) {
 
-    //int i = 0;
     for (int i=1 ; i<=100  ; i++ )
     {
 
-      if (i % 15 == 0){
+      if (i % 15 == 0){ //3でも5でも割り切れるとき
         System.out.print("FizzBuzz ");
-      }else if (i % 5 == 0){
+      }else if (i % 5 == 0){  //5で割り切れるとき
         System.out.print("Buzz ");
-      }else if (i % 3 == 0) {
+      }else if (i % 3 == 0) { //3で割り切れるとき
         System.out.print("Fizz ");
       }else{
         System.out.print(i + " ");
